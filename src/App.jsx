790c375896dcb092b489ed5813ccef7907e2f99b
Luis <lmendozag.lmmg@gmail.com>
2022-04-19 16:10:31 -0500

    Input Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
 
 function App () {
+  // const categories = ['One Punch', 'Dragon Ball', 'One Piece']
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball', 'One Piece']
+  )
+
+  const handleAdd = () => {
+    setCategories([...categories, 'My Little Darling'])
+  }
   return (
-    <h1>App</h1>
+    <>
+      <h2>GiftExpertApp</h2>
+      <AddCategory />
+      <hr />
+
+      <ol>
+        {categories.map((category) => {
+          return (
+            <li key={category}>
+              {category}
+            </li>
+          )
+        })}
+      </ol>
+    </>
   )
 }
 
